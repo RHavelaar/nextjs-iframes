734c1045b00df20efac86bee3a777c8a8b9f870c
Robert M Havelaar <Robert@MyBundle.tv>
2024-10-29 07:52:30 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,24 @@
 import Script from 'next/script'
 
-export default function EzeeFiberPage() {
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        {/* Find My Bundle */}
-        <section className="py-20">
-          {/* Find My Bundle: Testing iframe */}
-          <div id="find-my-bundle-test-wrapper">
-            <Script src="https://code.jquery.com/jquery-3.7.1.min.js" strategy="beforeInteractive" />
-            <Script src="https://app-stg-744688.mybundle.tv/js/iframeResizer.min.js" strategy="afterInteractive" />
-            <iframe
-              id="find-my-bundle-test"
-              style={{ border: 'none', overflow: 'hidden' }}
-              src="https://app-stg-744688.mybundle.tv/ezee/start?iframe"
-              height="550vh"
-              width="100%"
+export default function Test() {
+    return (
+        <>
+            <Script
+                src="https://code.jquery.com/jquery-3.7.1.min.js"
+                integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+                crossOrigin="anonymous"
             />
-            <Script src="https://app-stg-744688.mybundle.tv/js/ezee.js" strategy="afterInteractive" />
-          </div>
+            <div id="iframe-wrapper">
+                <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/iframeResizer.min.js" />
 
-          {/* Find My Bundle: Production iframe */}
-          <div id="find-my-bundle-prod-wrapper">
-            <Script src="https://mybundle.tv/js/iframeResizer.min.js" />
-            <iframe
-              id="find-my-bundle-prod"
-              style={{ border: 'none' }}
-              src="https://mybundle.tv/ezee/start?iframe"
-              height="550vh"
-              width="100%"
-              scrolling="no"
-            />
-            <Script src="https://mybundle.tv/js/ezee.js" />
-          </div>
-
-          {/* Marketplace iframes */}
-          {/* Marketplace: Testing iframe */}
-          <div id="marketplace-test-wrapper">
-            <Script src="https://app-stg-744688.mybundle.tv/js/iframeResizer.min.js" />
-            <iframe
-              id="marketplace-test"
-              style={{ border: 'none', overflow: 'hidden' }}
-              src="https://app-stg-744688.mybundle.tv/ezee/discover?iframe"
-              height="3700vh"
-              width="100%"
-              scrolling="no"
-            />
-            <Script src="https://app-stg-744688.mybundle.tv/js/ezee.js" />
-          </div>
-
-          {/* Marketplace: Production iframe */}
-          <div id="marketplace-prod-wrapper">
-            <Script src="https://mybundle.tv/js/iframeResizer.min.js" />
-            <iframe
-              id="marketplace-prod"
-              style={{ border: 'none' }}
-              src="https://mybundle.tv/ezee/discover?iframe"
-              height="3700vh"
-              width="100%"
-              scrolling="no"
-            />
-            <Script src="https://mybundle.tv/js/ezee.js" />
-          </div>
-
-          {/* Confirmation Page iframes */}
-          {/* Confirmation Page: Testing iframe */}
-          <div id="confirmation-test-wrapper">
-            <Script src="https://app-stg-744688.mybundle.tv/js/iframeResizer.min.js" />
-            <iframe
-              id="confirmation-test"
-              style={{ border: 'none' }}
-              src="https://app-stg-744688.mybundle.tv/ezee/isp-confirmation?iframe"
-              height="600vh"
-              width="100%"
-              scrolling="no"
-            />
-            <Script src="https://app-stg-744688.mybundle.tv/js/partner.js" />
-          </div>
-
-          {/* Confirmation Page: Production iframe */}
-          <div id="confirmation-prod-wrapper">
-            <Script src="https://mybundle.tv/js/iframeResizer.min.js" />
-            <iframe
-              id="confirmation-prod"
-              style={{ border: 'none' }}
-              src="https://mybundle.tv/ezee/isp-confirmation?iframe"
-              height="630vh"
-              width="100%"
-              scrolling="no"
-            />
-            <Script src="https://mybundle.tv/js/partner.js" />
-          </div>
-        </section>
-      </main>
-    </div>
-  )
+                <iframe
+                    id="myIframe"
+                    src="https://www-app-dev.mybundle.tv/ezee/start?iframe"
+                    scrolling="no"
+                    style={{ width: '100%', display: 'block' }}
+                ></iframe>
+                <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/ezee.js" />
+            </div>
+        </>
+    )
 }
