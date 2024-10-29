2dcd550014844972750d286737bbbeda793db2bc
Robert M Havelaar <Robert@MyBundle.tv>
2024-10-29 08:51:00 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import Script from 'next/script'
 
 export default function Test() {
-    return (
-        <>
-            <Script
-                src="https://code.jquery.com/jquery-3.7.1.min.js"
-                integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
-                crossOrigin="anonymous"
-            />
-            <div id="iframe-wrapper">
-                <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/iframeResizer.min.js" />
+  return (
+    <>
+      <Script
+        src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+        crossOrigin="anonymous"
+      />
+      <div id="iframe-wrapper">
+        <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/iframeResizer.min.js" />
 
-                <iframe
-                    id="myIframe"
-                    src="https://www-app-dev.mybundle.tv/ezee/start?iframe" // Change this to whatever iFrame you would like to load in.
-                    scrolling="no"
-                    style={{ width: '100%', display: 'block' }}
-                ></iframe>
-                <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/ezee.js" />
-            </div>
-        </>
-    )
+        <iframe
+          id="myIframe"
+          src="https://www-app-dev.mybundle.tv/ezee/start?iframe" // Change this to whatever iFrame you would like to load in.
+          scrolling="no"
+          style={{ width: '100%', display: 'block' }}
+        ></iframe>
+        <Script type="text/javascript" src="https://www-app-dev.mybundle.tv/js/ezee.js" />
+      </div>
+    </>
+  )
 }
